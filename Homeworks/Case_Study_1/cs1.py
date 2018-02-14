efdfe8a13b1a79d7e397785f518439d72fd0fbd4
Vladimir Lavrik <lavrikvladimir@gmail.com>
2018-02-14 08:48:55 +0300

    1st case study is added

---
+++ Homeworks/Case_Study_1/cs1.py
@@ -0,0 +1,21 @@
+# Let's look at the lowercase letters.
+import string
+alphabet = ' ' + string.ascii_lowercase # Add your code here! #
+positions = {}
+# Let's look at the lowercase letters.
+
+for i in range(27):
+    positions[alphabet[i]] = i
+
+message = "hi my name is caesar"
+
+def encode(message, key):
+	encoded_message = str()
+	item_list = list(positions.items())
+	for letter in message:
+	    print(letter, positions[letter], item_list[(positions[letter]+key)%27][0],
+	          item_list[(positions[letter]+key)%27][1] )
+	    encoded_message+=item_list[(positions[letter]+1)%27][0]
+	return encoded_message 
+encoded_message = encode(message, 10)
+print(encoded_message)
